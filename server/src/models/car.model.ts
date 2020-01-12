9972252d064ab615bf5c57569f95ac99651685ec
Paul Hovley <rphovley@gmail.com>
2020-01-12 15:37:07 -0700

    add some validation for enums

--- server/src/models/car.model.ts
+++ server/src/models/car.model.ts
@@ -1,3 +1,4 @@
+import { ValidationError } from 'objection'
 import { BaseModel } from './base.model'
 
 export enum CarType {
@@ -19,4 +20,25 @@ export class Car extends BaseModel implements ICar {
   year: string
   type: CarType
   static tableName = 'car'
+
+  $beforeInsert(): void {
+    super.$beforeInsert()
+    this.validateCarType()
+  }
+
+  $beforeUpdate(): void {
+    super.$beforeUpdate()
+    this.validateCarType()
+  }
+
+  private validateCarType = (): void => {
+    // if car type is not one of the types in the CarType enum
+    if (CarType[this.type] == undefined) {
+      throw new ValidationError({
+        message: 'The type is not a valid value (Three, S, X)',
+        type: 'CarTypeInvalidError',
+        data: this.toJSON(),
+      })
+    }
+  }
 }
--- server/src/models/price.model.ts
+++ server/src/models/price.model.ts
@@ -1,4 +1,4 @@
-import { Model } from 'objection'
+import { Model, ValidationError } from 'objection'
 import { BaseModel } from './base.model'
 import { CarType } from './car.model'
 import { Route } from './route.model'
@@ -25,5 +25,26 @@ export class Price extends BaseModel implements IPrice {
         to: 'route.id',
       },
     },
+  }
+
+  $beforeInsert(): void {
+    super.$beforeInsert()
+    this.validateCarType()
+  }
+
+  $beforeUpdate(): void {
+    super.$beforeUpdate()
+    this.validateCarType()
+  }
+
+  private validateCarType = (): void => {
+    // if car type is not one of the types in the CarType enum
+    if (CarType[this.car_type] == undefined) {
+      throw new ValidationError({
+        message: 'The type is not a valid value (Three, S, X)',
+        type: 'CarTypeInvalidError',
+        data: this.toJSON(),
+      })
+    }
   }
 }
